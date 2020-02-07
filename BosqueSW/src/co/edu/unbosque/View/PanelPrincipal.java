e981b8491240444c559dc92c6769fad871bf4a3c
JenniferColorado <49852404+JenniferColorado@users.noreply.github.com>
2020-02-07 17:36:31 -0500

    Update PanelPrincipal.java

--- BosqueSW/src/co/edu/unbosque/View/PanelPrincipal.java
+++ BosqueSW/src/co/edu/unbosque/View/PanelPrincipal.java
@@ -1,5 +1,16 @@
 package co.edu.unbosque.View;
 
+import java.awt.Image;
+
+import javax.swing.Icon;
+import javax.swing.JButton;
+
 public class PanelPrincipal {
 
+	private String ruta;
+	private Image fondo;
+    private JButton register, lista;
+    private Icon registrarIcon, ListaIcon;
+    private final String REGISTRAR = "REGISTRAR";
+    
 }
