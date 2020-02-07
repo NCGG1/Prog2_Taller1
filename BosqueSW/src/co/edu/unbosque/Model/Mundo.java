8d68458ff104bd8b3757c07a42b0d515fe9f78db
Juankpp <49852280+Juankpp@users.noreply.github.com>
2020-02-07 17:32:54 -0500

    Metodo actualizar

--- BosqueSW/src/co/edu/unbosque/Model/Mundo.java
+++ BosqueSW/src/co/edu/unbosque/Model/Mundo.java
@@ -2,75 +2,131 @@ package co.edu.unbosque.Model;
 
 import java.util.ArrayList;
 
-
 public class Mundo {
 
-	private ArrayList <Personal> empleados;
-	
+	private ArrayList<Personal> empleados;
+
 	public Mundo() {
 		// TODO Auto-generated constructor stub
 		empleados = new ArrayList<>();
 	}
-	
+
+	public String actualizarPersona(String nombre, String apellido, String genero, long telefono, String correo,
+			String direccion, String anoIngreso, long cedula) {
+
+		String actualizar = "";
+		if (existePersonal(cedula) == true) {
+
+			Personal personal = buscarPersona(cedula);
+
+			if (!nombre.equalsIgnoreCase(personal.getNombre())) {
+
+				personal.setNombre(nombre);
+				actualizar = "Se actualizo el personal";
+			}
+
+			if (!apellido.equalsIgnoreCase(personal.getApellido())) {
+
+				personal.setApellido(apellido);
+				actualizar = "Se actualizo el personal";
+
+			}
+
+			if (!genero.equalsIgnoreCase(personal.getGenero())) {
+				personal.setGenero(genero);
+				actualizar = "Se actualizo el personal";
+
+			}
+
+			if (telefono != personal.getTelefono()) {
+
+				personal.setTelefono(telefono);
+				actualizar = "Se actualizo el personal";
+
+			}
+
+			if (correo.equalsIgnoreCase(personal.getCorreo())) {
+				personal.setCorreo(correo);
+				actualizar = "Se actualizo el personal";
+
+			}
+
+			if (anoIngreso.equalsIgnoreCase(personal.getAnoIngreso())) {
+
+				personal.getAnoIngreso();
+				actualizar = "Se actualizo el personal";
+
+			}
+
+			if (direccion.equalsIgnoreCase(personal.getDireccion())) {
+
+				personal.setDireccion(direccion);
+				actualizar = "Se actualizo el personal";
+
+			}
+
+		} else {
+
+			actualizar = "No existe el personal";
+
+		}
+
+		return actualizar;
+	}
+
 	/**
 	 * 
 	 * @param cedula
 	 * @return
 	 */
 	public Personal buscarPersona(long cedula) {
-		
-	 Personal persona1 = null;
-	 
-	 for (int i = 0; i < empleados.size(); i++) {
-		if (empleados.get(i).getCedula()== cedula) {
-			persona1 = empleados.get(i);
+
+		Personal persona1 = null;
+
+		for (int i = 0; i < empleados.size(); i++) {
+			if (empleados.get(i).getCedula() == cedula) {
+				persona1 = empleados.get(i);
+			}
 		}
-	}
-	
+
 		return persona1;
 	}
-	
+
 	/**
 	 * 
 	 * @param cedula
 	 * @return
 	 */
 	public boolean existePersonal(long cedula) {
-		
+
 		boolean existePersona = false;
-		
-			if(buscarPersona(cedula) != null) {
-				existePersona = true;
-			}
-		
+
+		if (buscarPersona(cedula) != null) {
+			existePersona = true;
+		}
+
 		return existePersona;
 	}
 
-	
 	/**
 	 * 
 	 * @param cedula
 	 * @return
 	 */
-	public String borrarPersona (long cedula) {
-		
+	public String borrarPersona(long cedula) {
+
 		String oo = "";
-		
+
 		if (buscarPersona(cedula) != null) {
 			empleados.remove(buscarPersona(cedula));
 			oo = "Se eliminó a la persona correctamente ";
-		}
-		else {
+		} else {
 			oo = "No se eliminó a la persona correctamente ";
 		}
-		
-		
-		
-		return oo ;
+
+		return oo;
 	}
-	
-	
-	
+
 	/**
 	 * 
 	 * 
@@ -78,67 +134,60 @@ public class Mundo {
 	 * @return
 	 */
 	public String incrementarDatosVarios(long cedula) {
-		
-		
+
 		String respuesta = "";
-		
-		if(existePersonal(cedula)) {
-		Personal persona = buscarPersona(cedula);
-		
-		if(persona.getTipoIngeniero().equalsIgnoreCase("Ingeniero Junior")) {
-			
-			IngenieroJunior junior = (IngenieroJunior)persona;
-			
-			if(junior.getNivel() < 5) {
-				
-				junior.setNivel((short)(junior.getNivel() + 1));
-				junior.calcularSalario();
-				respuesta = "Nivel incrementado, felicitaciones ingeniero";
-			}else {
 
-				respuesta = "Lo sentimos ha alcanzado el nivel maximo";
-			}
-		}else if(persona.getTipoIngeniero().equalsIgnoreCase("Ingeniero Senior")) {
-			
-			IngenieroSenior senior = (IngenieroSenior)persona;
-			
-			senior.setVentas(senior.getVentas()+1);
-			respuesta = "Nueva venta, felicitaciones ingeniero";
-				
-		}else if(persona.getTipoIngeniero().equalsIgnoreCase("Ingeniero Comision")) {
-			
-			PersonalComision comision = (PersonalComision)persona;
-			
-			double[] clientesViejos = comision.getSalarios();
-			double[] clientesNuevos = new double[clientesViejos.length+1];
+		if (existePersonal(cedula)) {
+			Personal persona = buscarPersona(cedula);
 
-			for (int i = 0; i < clientesViejos.length; i++) {
-				
-			
-				clientesNuevos[i] = clientesViejos[i];
-				
-			}
-			//MATH RANDOM!!!! FALTA
-				clientesNuevos[clientesViejos.length-1] = 10000;
-				
+			if (persona.getTipoIngeniero().equalsIgnoreCase("Ingeniero Junior")) {
+
+				IngenieroJunior junior = (IngenieroJunior) persona;
+
+				if (junior.getNivel() < 5) {
+
+					junior.setNivel((short) (junior.getNivel() + 1));
+					junior.calcularSalario();
+					respuesta = "Nivel incrementado, felicitaciones ingeniero";
+				} else {
+
+					respuesta = "Lo sentimos ha alcanzado el nivel maximo";
+				}
+			} else if (persona.getTipoIngeniero().equalsIgnoreCase("Ingeniero Senior")) {
+
+				IngenieroSenior senior = (IngenieroSenior) persona;
+
+				senior.setVentas(senior.getVentas() + 1);
+				respuesta = "Nueva venta, felicitaciones ingeniero";
+
+			} else if (persona.getTipoIngeniero().equalsIgnoreCase("Ingeniero Comision")) {
+
+				PersonalComision comision = (PersonalComision) persona;
+
+				double[] clientesViejos = comision.getSalarios();
+				double[] clientesNuevos = new double[clientesViejos.length + 1];
+
+				for (int i = 0; i < clientesViejos.length; i++) {
+
+					clientesNuevos[i] = clientesViejos[i];
+
+				}
+				// MATH RANDOM!!!! FALTA
+				clientesNuevos[clientesViejos.length - 1] = 10000;
+
 				comision.setSalarios(clientesNuevos);
 				comision.calcularSalario();
 				respuesta = "Nuevo cliente, felicitaciones ingeniero";
-		}
-		
-		
-		}else {
-			
+			}
+
+		} else {
+
 			respuesta = "No existe ningun ingeniero con la cedula ingresada";
-			
+
 		}
-		
+
 		return respuesta;
-		
+
 	}
 
-	
-	
-	
 }
-	
